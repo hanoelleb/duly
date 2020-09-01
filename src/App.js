8893051a85636c776ad50de2e51135ef031c70c7
hanoelleb <hanoelleb@gmail.com>
2020-09-01 14:47:13 -0700

    started routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+
+const Notenav = () => {
+    return (
+    <div className='nav'>
+        <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/notes'>Notebooks</Link></li>
+            <li><Link to='/sign-in'>Sign in</Link></li>
+        </ul>
+    </div>
+    );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	<Router>
+	   <Notenav />
+	   <Switch>
+	       <Route exact path='/'>
+	           <h1>Duly</h1>
+	       </Route>
+
+	       <Route path='/notes'>
+                   <h1>Notebooks</h1>
+               </Route>
+
+	       <Route path='/sign-in'>
+                   <h1>Sign in</h1>
+               </Route>
+	   </Switch>
+	</Router>
     </div>
   );
 }
