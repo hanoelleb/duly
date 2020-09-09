83878aa73169bb04b16952c6756fe2d1f5d8b3da
hanoelleb <hanoelleb@gmail.com>
2020-09-08 22:30:59 -0700

    Updates

---
+++ precache-manifest.120144b79f28620096e500b7fea1104c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "432f065e85c4949a6f6a8c8719d60340",
+    "url": "/duly/index.html"
+  },
+  {
+    "revision": "1694dcd627c2c6b01592",
+    "url": "/duly/static/css/main.e0576793.chunk.css"
+  },
+  {
+    "revision": "e5eef9cbf24013b20b7f",
+    "url": "/duly/static/js/2.a43821aa.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/duly/static/js/2.a43821aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1694dcd627c2c6b01592",
+    "url": "/duly/static/js/main.25b259e8.chunk.js"
+  },
+  {
+    "revision": "8902116e6bf8f336e941",
+    "url": "/duly/static/js/runtime-main.4180c800.js"
+  }
+]);
